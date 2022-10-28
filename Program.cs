61ddcb269a2c3de02c48dc4ccc2e6857420acf34
Kieran Pringle <kieran.pringle@bbc.co.uk>
2022-10-28 09:53:44 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SadConsole;
+using SadRogue.Primitives;
+using Console = SadConsole.Console;
+
+namespace SadConsoleGame
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            // Setup the engine and create the main window.
+            Game.Create(120, 38);
+
+            // Hook the start event so we can add consoles to the system.
+            Game.Instance.OnStart = Init;
+
+            // Start the game.
+            Game.Instance.Run();
+            Game.Instance.Dispose();
+        }
+
+        static void Init()
+        {
+
+        }
+    }
+}
